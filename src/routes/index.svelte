8d06bb9de0e22c6f45f7b0404e005aadb0b9a428
dmac <samuraidan@gmail.com>
2021-12-13 19:42:40 -0800

    Better svelte store subscribe/unsubscribe behavior

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,3 @@
-<script context="module">
-	let firstTime = true;
-</script>
-
 <script lang="ts">
 	export const prerender = true;
 
@@ -51,35 +47,29 @@
 	 * 
 	 **************************************************************************/
 
-	/**
-	 * We only want to subscribe to keystore changes the very first time.
-	 * context = "module" (where we initialize firstTime) is only called at startup
-	*/
-	if (firstTime) {
-		firstTime = false;
-		keyStore.subscribe((store) => {
-			console.log(`subscribe changed ${isLoggedIn} ${store.isLoggedIn}`);
-			if (isLoggedIn !== store.isLoggedIn) {
-				isLoggedIn = store.isLoggedIn;
+	let unsubscribe = keyStore.subscribe((store) => {
+		console.log(`subscribe changed ${isLoggedIn} ${store.isLoggedIn}`);
+		if (isLoggedIn !== store.isLoggedIn) {
+			isLoggedIn = store.isLoggedIn;
+			if ($keyStore.weaveMailInboxThreads.length == 0 && !isLoadingMessages) {
+				pageStartupLogic();
+				//getLatestVersionTxid(arweave);
+			}
+		} else if (keys != store.keys) {
+			keys = store.keys;
+			if (keys == null) {
+				$keyStore.inboxThreads = [];
+			} else {
+				isLoggedIn = true;
+				$keyStore.isLoggedIn = true;
+				console.log("isLoadingMessages:" + isLoadingMessages)
 				if ($keyStore.weaveMailInboxThreads.length == 0 && !isLoadingMessages) {
 					pageStartupLogic();
-					//getLatestVersionTxid(arweave);
 				}
-			} else if (keys != store.keys) {
-				keys = store.keys;
-				if (keys == null) {
-					$keyStore.inboxThreads = [];
-				} else {
-					isLoggedIn = true;
-					$keyStore.isLoggedIn = true;
-					console.log("isLoadingMessages:" + isLoadingMessages)
-					if ($keyStore.weaveMailInboxThreads.length == 0 && !isLoadingMessages) {
-						pageStartupLogic();
-					}
-				}
-			} 
-		});
-	}
+			}
+		} 
+	});
+	
 	
 	/**
 	 * OnMount gets called every time we return to the index
@@ -104,6 +94,7 @@
 		} else {
 			console.log("logged out");
 			_inboxThreads = [];
+			unsubscribe();
 			return;
 		}
 
@@ -757,7 +748,7 @@ Thanks for checking out our project 💌
 		left: -0.7em;
 		width: 0.5em;
 		height: 0.5em;
-		background: url("/static/unread.svg") center/100% no-repeat;
+		background: url("unread.svg") center/100% no-repeat;
 		/* background-image: url("/src/lib/header/back-icon.svg"); */
 	}
 
@@ -811,7 +802,7 @@ Thanks for checking out our project 💌
 		left: 7em;
 		z-index: 10;
 		background: center / 1em no-repeat;
-		background-image: url("/static/email.svg");
+		background-image: url("email.svg");
 		/* filter: invert(39%) sepia(91%) saturate(809%) hue-rotate(190deg) brightness(101%) contrast(105%); */
 		filter: invert(99%) sepia(70%) saturate(310%) hue-rotate(294deg)
 			brightness(65%) contrast(85%);
@@ -827,7 +818,7 @@ Thanks for checking out our project 💌
 		left: 90px;
 		z-index: 10;
 		background: center / 1em no-repeat;
-		background-image: url("/static/identity2.svg");
+		background-image: url("identity2.svg");
 		filter: invert(46%) sepia(49%) saturate(835%) hue-rotate(204deg)
 			brightness(100%) contrast(114%);
 		/* filter: invert(99%) sepia(70%) saturate(310%) hue-rotate(294deg)
